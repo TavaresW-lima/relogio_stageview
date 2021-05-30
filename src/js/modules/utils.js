68aec02eae64a956ebad82d3f2c0a86a381c9891
William Tavares <tavareswilliamlima@gmail.com>
2021-05-29 21:51:26 -0300

    separação enums de utils

--- src/js/modules/utils.js
+++ src/js/modules/utils.js
@@ -1,3 +1,5 @@
+import {NOME_MES_ENUM,DIA_SEMANA_ENUM} from '../model/enums.js';
+
 function diaFormatado(data) {
     let dataMes = data.getDate();
     let dia = _diaParaTexto(data.getDay() + 1);
@@ -36,31 +38,6 @@ function _diaParaTexto(dia) {
             return DIA_SEMANA_ENUM[chave];
         }
     }
-}
-
-const NOME_MES_ENUM = {
-    1 : "Janeiro",
-    2 : "Fevereiro",
-    3 : "Março",
-    4 : "Abril",
-    5 : "Maio",
-    6 : "Junho",
-    7 : "Julho",
-    8 : "Agosto",
-    9 : "Setembro",
-    10 : "Outubro",
-    11 : "Novembro",
-    12 : "Dezembro" 
-}
-
-const DIA_SEMANA_ENUM = {
-    1 : "Domingo",
-    2 : "Segunda-Feira",
-    3 : "Terça-Feira",
-    4 : "Quarta-Feira",
-    5 : "Quinta-Feira",
-    6 : "Sexta-Feira",
-    7 : "Sábado" 
 }
 
 export {diaFormatado, horaFormatada}
